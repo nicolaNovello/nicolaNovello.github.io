e1df57946ac167d7087722e103cc60c44334cc13
nicolaNovello <83983292+nicolaNovello@users.noreply.github.com>
2026-07-04 06:36:37 +0000

    Deploying to gh-pages from @ nicolaNovello/nicolaNovello.github.io@4b56ccd7b30af845f0e718dd40ff7cc989fbaf1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
